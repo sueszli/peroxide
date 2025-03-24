bf8b6e208dfb756ba4bfb991f2022e9592d891ba
sueszli <jabaryyahya@gmail.com>
2025-03-24 01:27:08 +0100

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,32 @@
-// use wasm_bindgen::prelude::*;
-
-// #[wasm_bindgen]
-// extern "C" {
-//     pub fn alert(s: &str);
-// }
-
-// #[wasm_bindgen]
-// pub fn greet(name: &str) {
-//     alert(&format!("Hello, {}!", name));
-// }
-
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen(start)]
-fn run() {
-    bare_bones();
-    using_a_macro();
-    using_web_sys();
-}
+use web_sys::console;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
-
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-}
-
-fn bare_bones() {
-    log("Hello from Rust!");
-    log_u32(42);
-    log_many("Logging", "many values!");
 }
 
 macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-fn using_a_macro() {
+fn bare_bones() {
+    log("Hello from Rust!");
+
+    let js: JsValue = 4.into();
+    console::log_2(&"Logging arbitrary values looks like".into(), &js);
+
     console_log!("Hello {}!", "world");
+}
+
+fn using_a_macro() {
     console_log!("Let's print some numbers...");
     console_log!("1 + 3 = {}", 1 + 3);
 }
 
-fn using_web_sys() {
-    use web_sys::console;
-
-    console::log_1(&"Hello using web-sys".into());
-
-    let js: JsValue = 4.into();
-    console::log_2(&"Logging arbitrary values looks like".into(), &js);
+#[wasm_bindgen]
+pub fn run() {
+    bare_bones();
+    using_a_macro();
 }

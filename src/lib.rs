f33c3bf65cb070eff2664f064809d1f5fc40db84
sueszli <jabaryyahya@gmail.com>
2025-03-24 02:48:52 +0100

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,70 @@
+use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use web_sys::{Element, Event, HtmlElement, MouseEvent};
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+fn mount_window_size_listener(document: &web_sys::Document) -> Result<(), JsValue> {
+    let elem = document.create_element("div")?;
+    document.body().unwrap().append_child(&elem)?;
+
+    fn update_dimensions(elem: &Element) -> Result<(), JsValue> {
+        let width = web_sys::window().unwrap().inner_width()?.as_f64().unwrap();
+        let height = web_sys::window().unwrap().inner_height()?.as_f64().unwrap();
+        elem.set_text_content(Some(&format!("window size: {:.0} x {:.0}", width, height)));
+
+        Ok(())
+    }
+
+    update_dimensions(&elem)?; // initial update
+
+    let winsize_listener = Closure::<dyn FnMut(Event)>::new(move |_| {
+        update_dimensions(&elem).unwrap_throw();
+    });
+    web_sys::window()
+        .unwrap()
+        .add_event_listener_with_callback("resize", winsize_listener.as_ref().unchecked_ref())?;
+
+    winsize_listener.forget(); // don't drop
+
+    Ok(())
 }
 
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
+fn mount_mousemove_listener(document: &web_sys::Document) -> Result<(), JsValue> {
+    let elem = document.create_element("div")?;
+    document.body().unwrap().append_child(&elem)?;
 
-fn bare_bones() {
-    log("Hello from Rust!");
+    fn update_mouse_position(elem: &Element, event: &web_sys::MouseEvent) -> Result<(), JsValue> {
+        let x = event.client_x();
+        let y = event.client_y();
+        elem.set_text_content(Some(&format!("mouse position: {:.0} x {:.0}", x, y)));
 
-    let js: JsValue = 4.into();
-    console::log_2(&"Logging arbitrary values looks like".into(), &js);
+        Ok(())
+    }
 
-    console_log!("1 + 3 = {}", 1 + 3);
+    let mousemove_listener = Closure::<dyn FnMut(web_sys::MouseEvent)>::new(move |event| {
+        update_mouse_position(&elem, &event).unwrap_throw();
+    });
+    web_sys::window()
+        .unwrap()
+        .add_event_listener_with_callback(
+            "mousemove",
+            mousemove_listener.as_ref().unchecked_ref(),
+        )?;
+
+    mousemove_listener.forget(); // don't drop
+
+    Ok(())
 }
 
 #[wasm_bindgen(start)]
-pub fn run() {
-    // called on initial load
-    bare_bones();
+pub fn run() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once(); // panics to console.error
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
+
+    mount_window_size_listener(&document)?;
+    mount_mousemove_listener(&document)?;
+
+    Ok(())
 }

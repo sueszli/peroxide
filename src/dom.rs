767dc2f648e94db255b97ed453fee457ca68b40c
sueszli <jabaryyahya@gmail.com>
2025-08-17 13:04:46 +0200

    up

--- src/dom.rs
+++ src/dom.rs
@@ -21,18 +21,3 @@ pub fn onclick<F: 'static + FnMut()>(element: &Element, function: F) {
     element.add_event_listener_with_callback("click", callback.as_ref().unchecked_ref()).unwrap();
     callback.forget();
 }
-
-/// Gets or sets the inner text content of an element by its ID.
-///
-/// For HTML: `<div id="myDiv">hello</div>`
-/// ```rust
-/// let text = get_elem_innertext("myDiv"); // Returns Some("hello")
-/// ```
-pub fn get_elem_innertext(id: &str) -> Option<String> {
-    document().get_element_by_id(id)?.text_content()
-}
-pub fn set_elem_innertext(id: &str, text: &str) {
-    if let Some(element) = document().get_element_by_id(id) {
-        element.set_text_content(Some(text));
-    }
-}

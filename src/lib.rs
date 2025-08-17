4b80934e43c367f50133be029b66797e83989143
sueszli <jabaryyahya@gmail.com>
2025-08-17 23:12:56 +0200

    abstract over entire protocol - now comes the fun stuff

--- src/lib.rs
+++ src/lib.rs
@@ -11,169 +11,6 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::*;
 use web_sys::*;
 
-#[derive(Clone, Copy)]
-pub struct P2PCallbacks {
-    pub on_connection_established: fn(Rc<RefCell<Option<p2p::PeerConnection>>>),
-    pub on_message: fn(String),
-}
-
-const HOST_HTML: &str = r#"
-    <div>
-        <h2>Host</h2>
-
-        <p>Send this invite code to your guest:</p>
-
-        <textarea id="my_sdp" readonly></textarea>
-
-        <p>Enter your guest's response code:</p>
-
-        <textarea id="peer_sdp" placeholder="Enter here"></textarea>
-        <button id="connect">Connect</button>
-    </div>
-    <style>
-        p {
-            margin-top: 2rem;
-            margin-bottom: 2rem;
-        }
-        h2 {
-            margin-top: 6rem;
-        }
-        textarea {
-            width: 100%;
-            height: 4rem;
-        }
-        button {
-            cursor: pointer;
-            font-family: 'Lucida Console', monospace;
-            padding: 0.5rem;
-
-            background-color: #f0f0f0;
-            border: 1px solid;
-            width: 20%;
-        }
-    </style>
-"#;
-pub fn render_host(callbacks: P2PCallbacks) {
-    dom::document().body().unwrap().set_inner_html(HOST_HTML);
-
-    let peer_connection = Rc::new(RefCell::new(None));
-
-    // stage 1: create offer
-    let peer_connection2 = peer_connection.clone();
-    let peer_connection3 = peer_connection.clone();
-    let p2p_callbacks = p2p::PeerConnectionCallbacks {
-        on_sdp_ready: Box::new(|json| {
-            let elem = dom::document().get_element_by_id("my_sdp").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap();
-            let sdp_str = utils::compress_string(&json);
-            elem.set_inner_html(&sdp_str);
-        }),
-        on_connection_status_change: Box::new(|state_str| view::update_connection_notification(&state_str)),
-        on_connection_established: Box::new(move || {
-            view::update_notification("Connection established successfully!");
-            (callbacks.on_connection_established)(peer_connection3.clone());
-        }),
-        on_message_received: Box::new(move |msg| {
-            (callbacks.on_message)(msg);
-        }),
-    };
-    let pc = p2p::create_host_peer_connection(p2p_callbacks);
-    wasm_bindgen_futures::spawn_local(async move {
-        pc.create_offer().await.unwrap();
-        view::update_notification("Created invite code! Share it with your Guest.");
-        *peer_connection2.borrow_mut() = Some(pc);
-    });
-
-    // stage 3: receive answer
-    let btn = dom::document().get_element_by_id("connect").unwrap();
-    dom::onclick(&btn, move || {
-        let elem = dom::document().get_element_by_id("peer_sdp").unwrap();
-        let content = elem.dyn_into::<HtmlTextAreaElement>().unwrap().value();
-        let sdp_str = utils::decompress_string(&content);
-
-        let peer_connection4 = peer_connection.clone();
-        wasm_bindgen_futures::spawn_local(async move {
-            if let Some(con) = peer_connection4.borrow().as_ref() {
-                con.set_remote_description(&sdp_str).await.unwrap();
-                view::update_notification("Attempting to establish connection...");
-            }
-        });
-    });
-}
-
-const GUEST_HTML: &str = r#"
-    <div>
-        <h2>Guest</h2>
-
-        <p>Enter your host's invite code:</p>
-
-        <textarea id="peer_sdp" placeholder="Enter here"></textarea>
-        <button id="connect">Connect</button>
-        
-        <p>Send this response code to your host:</p>
-
-        <textarea id="my_sdp" readonly></textarea>
-    </div>
-    <style>
-        p {
-            margin-top: 2rem;
-            margin-bottom: 2rem;
-        }
-        h2 {
-            margin-top: 6rem;
-        }
-        textarea {
-            width: 100%;
-            height: 4rem;
-        }
-        button {
-            cursor: pointer;
-            font-family: 'Lucida Console', monospace;
-            padding: 0.5rem;
-
-            background-color: #f0f0f0;
-            border: 1px solid;
-            width: 20%;
-        }
-    </style>
-"#;
-pub fn render_guest(callbacks: P2PCallbacks) {
-    dom::document().body().unwrap().set_inner_html(GUEST_HTML);
-
-    // stage 2: create answer
-    let peer_connection = Rc::new(RefCell::new(None));
-
-    let btn = dom::document().get_element_by_id("connect").unwrap();
-    dom::onclick(&btn, move || {
-        let elem = dom::document().get_element_by_id("peer_sdp").unwrap();
-        let content = elem.dyn_into::<HtmlTextAreaElement>().unwrap().value();
-        let sdp_str = utils::decompress_string(&content);
-
-        let peer_connection2 = peer_connection.clone();
-        let peer_connection3 = peer_connection.clone();
-
-        wasm_bindgen_futures::spawn_local(async move {
-            let p2p_callbacks = p2p::PeerConnectionCallbacks {
-                on_sdp_ready: Box::new(|json| {
-                    let elem = dom::document().get_element_by_id("my_sdp").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap();
-                    let sdp_str = utils::compress_string(&json);
-                    elem.set_inner_html(&sdp_str);
-                }),
-                on_connection_status_change: Box::new(|state| view::update_connection_notification(&state)),
-                on_connection_established: Box::new(move || {
-                    view::update_notification("Connection established successfully!");
-                    (callbacks.on_connection_established)(peer_connection3.clone());
-                }),
-                on_message_received: Box::new(move |msg| {
-                    (callbacks.on_message)(msg);
-                }),
-            };
-            let peer_conn = p2p::create_guest_peer_connection(&sdp_str, p2p_callbacks).await.unwrap();
-            *peer_connection2.borrow_mut() = Some(peer_conn);
-            view::update_notification("Created response code! Share it with your host.");
-        });
-    });
-}
-
 const CHAT_HTML: &str = r#"
     <div>
         <h2>Chat</h2>
@@ -223,13 +60,8 @@ const CHAT_HTML: &str = r#"
 pub fn render_chat(peer_connection: Rc<RefCell<Option<p2p::PeerConnection>>>) {
     dom::document().body().unwrap().set_inner_html(CHAT_HTML);
 
-    fn get_message() -> String {
-        dom::document().get_element_by_id("message").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap().value()
-    }
-
-    fn clear_message() {
-        dom::document().get_element_by_id("message").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap().set_value("")
-    }
+    let get_message = || dom::document().get_element_by_id("message").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap().value();
+    let clear_message = || dom::document().get_element_by_id("message").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap().set_value("");
 
     let text_area = dom::document().get_element_by_id("message").unwrap();
     let con_clone = peer_connection.clone();
@@ -274,14 +106,13 @@ pub fn run() -> Result<(), JsValue> {
 
     view::init();
 
-    let callbacks = P2PCallbacks {
+    let callbacks = view::P2PCallbacks {
         on_connection_established: |peer_connection| render_chat(peer_connection),
         on_message: |msg| {
             append_log(&format!("Peer: {}", msg));
         },
     };
-
-    view::render_role_selection(move || render_host(callbacks), move || render_guest(callbacks));
+    view::render_role_selection(move || view::render_host(callbacks), move || view::render_guest(callbacks));
 
     Ok(())
 }
--- src/view.rs
+++ src/view.rs
@@ -2,6 +2,15 @@
 //! They work by replacing the entire `body` element with a new one.
 
 use crate::dom;
+use crate::p2p;
+use crate::utils;
+
+use std::{cell::RefCell, rc::Rc};
+
+use js_sys::*;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::*;
+use web_sys::*;
 
 const GLOBAL_STYLING: &str = r#"
     * { margin: 0; padding: 0; }
@@ -163,4 +172,167 @@ pub fn render_role_selection(on_host_selection: impl Fn() + 'static, on_guest_se
 
     let guest_btn = dom::document().get_element_by_id("guest_selection").unwrap();
     dom::onclick(&guest_btn, move || on_guest_selection());
+}
+
+#[derive(Clone, Copy)]
+pub struct P2PCallbacks {
+    pub on_connection_established: fn(Rc<RefCell<Option<p2p::PeerConnection>>>),
+    pub on_message: fn(String),
+}
+
+const HOST_HTML: &str = r#"
+    <div>
+        <h2>Host</h2>
+
+        <p>Send this invite code to your guest:</p>
+
+        <textarea id="my_sdp" readonly></textarea>
+
+        <p>Enter your guest's response code:</p>
+
+        <textarea id="peer_sdp" placeholder="Enter here"></textarea>
+        <button id="connect">Connect</button>
+    </div>
+    <style>
+        p {
+            margin-top: 2rem;
+            margin-bottom: 2rem;
+        }
+        h2 {
+            margin-top: 6rem;
+        }
+        textarea {
+            width: 100%;
+            height: 4rem;
+        }
+        button {
+            cursor: pointer;
+            font-family: 'Lucida Console', monospace;
+            padding: 0.5rem;
+
+            background-color: #f0f0f0;
+            border: 1px solid;
+            width: 20%;
+        }
+    </style>
+"#;
+pub fn render_host(callbacks: P2PCallbacks) {
+    dom::document().body().unwrap().set_inner_html(HOST_HTML);
+
+    let peer_connection = Rc::new(RefCell::new(None));
+
+    // stage 1: create offer
+    let peer_connection2 = peer_connection.clone();
+    let peer_connection3 = peer_connection.clone();
+    let p2p_callbacks = p2p::PeerConnectionCallbacks {
+        on_sdp_ready: Box::new(|json| {
+            let elem = dom::document().get_element_by_id("my_sdp").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap();
+            let sdp_str = utils::compress_string(&json);
+            elem.set_inner_html(&sdp_str);
+        }),
+        on_connection_status_change: Box::new(|state_str| update_connection_notification(&state_str)),
+        on_connection_established: Box::new(move || {
+            update_notification("Connection established successfully!");
+            (callbacks.on_connection_established)(peer_connection3.clone());
+        }),
+        on_message_received: Box::new(move |msg| {
+            (callbacks.on_message)(msg);
+        }),
+    };
+    let pc = p2p::create_host_peer_connection(p2p_callbacks);
+    wasm_bindgen_futures::spawn_local(async move {
+        pc.create_offer().await.unwrap();
+        *peer_connection2.borrow_mut() = Some(pc);
+        update_notification("Created invite code! Share it with your Guest.");
+    });
+
+    // stage 3: receive answer
+    let btn = dom::document().get_element_by_id("connect").unwrap();
+    dom::onclick(&btn, move || {
+        let elem = dom::document().get_element_by_id("peer_sdp").unwrap();
+        let content = elem.dyn_into::<HtmlTextAreaElement>().unwrap().value();
+        let sdp_str = utils::decompress_string(&content);
+
+        let peer_connection4 = peer_connection.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            if let Some(con) = peer_connection4.borrow().as_ref() {
+                con.set_remote_description(&sdp_str).await.unwrap();
+                update_notification("Attempting to establish connection...");
+            }
+        });
+    });
+}
+
+const GUEST_HTML: &str = r#"
+    <div>
+        <h2>Guest</h2>
+
+        <p>Enter your host's invite code:</p>
+
+        <textarea id="peer_sdp" placeholder="Enter here"></textarea>
+        <button id="connect">Connect</button>
+        
+        <p>Send this response code to your host:</p>
+
+        <textarea id="my_sdp" readonly></textarea>
+    </div>
+    <style>
+        p {
+            margin-top: 2rem;
+            margin-bottom: 2rem;
+        }
+        h2 {
+            margin-top: 6rem;
+        }
+        textarea {
+            width: 100%;
+            height: 4rem;
+        }
+        button {
+            cursor: pointer;
+            font-family: 'Lucida Console', monospace;
+            padding: 0.5rem;
+
+            background-color: #f0f0f0;
+            border: 1px solid;
+            width: 20%;
+        }
+    </style>
+"#;
+pub fn render_guest(callbacks: P2PCallbacks) {
+    dom::document().body().unwrap().set_inner_html(GUEST_HTML);
+
+    // stage 2: create answer
+    let peer_connection = Rc::new(RefCell::new(None));
+
+    let btn = dom::document().get_element_by_id("connect").unwrap();
+    dom::onclick(&btn, move || {
+        let elem = dom::document().get_element_by_id("peer_sdp").unwrap();
+        let content = elem.dyn_into::<HtmlTextAreaElement>().unwrap().value();
+        let sdp_str = utils::decompress_string(&content);
+
+        let peer_connection2 = peer_connection.clone();
+        let peer_connection3 = peer_connection.clone();
+
+        wasm_bindgen_futures::spawn_local(async move {
+            let p2p_callbacks = p2p::PeerConnectionCallbacks {
+                on_sdp_ready: Box::new(|json| {
+                    let elem = dom::document().get_element_by_id("my_sdp").unwrap().dyn_into::<HtmlTextAreaElement>().unwrap();
+                    let sdp_str = utils::compress_string(&json);
+                    elem.set_inner_html(&sdp_str);
+                }),
+                on_connection_status_change: Box::new(|state| update_connection_notification(&state)),
+                on_connection_established: Box::new(move || {
+                    update_notification("Connection established successfully!");
+                    (callbacks.on_connection_established)(peer_connection3.clone());
+                }),
+                on_message_received: Box::new(move |msg| {
+                    (callbacks.on_message)(msg);
+                }),
+            };
+            let peer_conn = p2p::create_guest_peer_connection(&sdp_str, p2p_callbacks).await.unwrap();
+            *peer_connection2.borrow_mut() = Some(peer_conn);
+            update_notification("Created response code! Share it with your host.");
+        });
+    });
 }

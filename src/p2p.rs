f587a49d1b52a96f16a85a6ad4ab87350db588b5
sueszli <jabaryyahya@gmail.com>
2025-08-18 23:25:43 +0200

    up

--- src/p2p.rs
+++ src/p2p.rs
@@ -29,12 +29,17 @@
 //! - [RFC 3264: An Offer/Answer Model with SDP](https://datatracker.ietf.org/doc/html/rfc3264)
 //! - [WebRTC API Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API)
 
+use crate::utils::{Kestrel, Thrush};
 use js_sys;
 use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::{JsCast, JsValue, prelude::*};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Event, MessageEvent, RtcConfiguration, RtcDataChannel, RtcDataChannelEvent, RtcDataChannelState, RtcIceServer, RtcPeerConnection, RtcPeerConnectionIceEvent, RtcPeerConnectionState};
+use web_sys::{
+    Event, MessageEvent, RtcConfiguration, RtcDataChannel, RtcDataChannelEvent,
+    RtcDataChannelState, RtcIceServer, RtcPeerConnection, RtcPeerConnectionIceEvent,
+    RtcPeerConnectionState,
+};
 
 type StringCallback = Box<dyn FnMut(String)>;
 type StatusCallback = Box<dyn FnMut(&'static str)>;
@@ -58,7 +63,9 @@ impl PeerConnection {
         self.dc
             .borrow()
             .as_ref()
-            .filter(|dc| dc.ready_state() == RtcDataChannelState::Open && !message.trim().is_empty())
+            .filter(|dc| {
+                dc.ready_state() == RtcDataChannelState::Open && !message.trim().is_empty()
+            })
             .map(|dc| {
                 dc.send_with_str(message).ok();
                 console_log!("message sent: {}", message);
@@ -81,24 +88,42 @@ impl PeerConnection {
 }
 
 pub fn create_host_peer_connection(callbacks: PeerConnectionCallbacks) -> PeerConnection {
-    create_rtc_peer_connection().pipe(|pc| (pc.create_data_channel("app"), pc)).pipe(|(dc, pc)| {
-        let dc_ref = Rc::new(RefCell::new(Some(dc.clone())));
-        setup_callbacks(&pc, Some(&dc), callbacks);
-        PeerConnection { pc, dc: dc_ref }
-    })
+    create_rtc_peer_connection()
+        .pipe(|pc| (pc.create_data_channel("app"), pc))
+        .pipe(|(dc, pc)| {
+            let dc_ref = Rc::new(RefCell::new(Some(dc.clone())));
+            setup_callbacks(&pc, Some(&dc), callbacks);
+            PeerConnection { pc, dc: dc_ref }
+        })
 }
 
-pub async fn create_guest_peer_connection(offer: &str, callbacks: PeerConnectionCallbacks) -> Result<PeerConnection, JsValue> {
+pub async fn create_guest_peer_connection(
+    offer: &str,
+    callbacks: PeerConnectionCallbacks,
+) -> Result<PeerConnection, JsValue> {
     let dc_ref = Rc::new(RefCell::new(None));
 
     create_rtc_peer_connection()
         .tap(|pc| setup_ice_callback(pc, callbacks.on_sdp_ready))
         .tap(|pc| setup_connection_state_callback(pc, callbacks.on_connection_status_change))
-        .tap(|pc| setup_guest_data_channel(pc, dc_ref.clone(), callbacks.on_connection_established, callbacks.on_message_received).unwrap())
+        .tap(|pc| {
+            setup_guest_data_channel(
+                pc,
+                dc_ref.clone(),
+                callbacks.on_connection_established,
+                callbacks.on_message_received,
+            )
+            .unwrap()
+        })
         .pipe(|pc| async move {
-            js_sys::JSON::parse(offer)?.pipe(|sdp| JsFuture::from(pc.set_remote_description(&sdp.into()))).await?;
+            js_sys::JSON::parse(offer)?
+                .pipe(|sdp| JsFuture::from(pc.set_remote_description(&sdp.into())))
+                .await?;
 
-            JsFuture::from(pc.create_answer()).await?.pipe(|answer| JsFuture::from(pc.set_local_description(&answer.into()))).await?;
+            JsFuture::from(pc.create_answer())
+                .await?
+                .pipe(|answer| JsFuture::from(pc.set_local_description(&answer.into())))
+                .await?;
 
             Ok(PeerConnection { pc, dc: dc_ref })
         })
@@ -106,55 +131,48 @@ pub async fn create_guest_peer_connection(offer: &str, callbacks: PeerConnection
 }
 
 fn create_rtc_peer_connection() -> RtcPeerConnection {
-    RtcIceServer::new().tap(|s| s.set_urls(&js_sys::Array::of1(&JsValue::from_str("stun:stun.l.google.com:19302")))).pipe(|ice_server| RtcConfiguration::new().tap(|c| c.set_ice_servers(&js_sys::Array::of1(&ice_server)))).pipe(|config| RtcPeerConnection::new_with_configuration(&config).unwrap())
+    RtcIceServer::new()
+        .tap(|s| {
+            s.set_urls(&js_sys::Array::of1(&JsValue::from_str(
+                "stun:stun.l.google.com:19302",
+            )))
+        })
+        .pipe(|ice_server| {
+            RtcConfiguration::new().tap(|c| c.set_ice_servers(&js_sys::Array::of1(&ice_server)))
+        })
+        .pipe(|config| RtcPeerConnection::new_with_configuration(&config).unwrap())
 }
 
-trait Tap<T> {
-    fn tap<F>(self, f: F) -> Self
-    where
-        F: FnOnce(&Self);
-}
+fn setup_callbacks(
+    pc: &RtcPeerConnection,
+    dc: Option<&RtcDataChannel>,
+    callbacks: PeerConnectionCallbacks,
+) {
+    pc.tap(|pc| setup_ice_callback(pc, callbacks.on_sdp_ready))
+        .tap(|pc| setup_connection_state_callback(pc, callbacks.on_connection_status_change));
 
-trait Pipe<T> {
-    fn pipe<U, F>(self, f: F) -> U
-    where
-        F: FnOnce(Self) -> U,
-        Self: Sized;
-}
-
-impl<T> Tap<T> for T {
-    fn tap<F>(self, f: F) -> Self
-    where
-        F: FnOnce(&Self),
-    {
-        f(&self);
-        self
-    }
-}
-
-impl<T> Pipe<T> for T {
-    fn pipe<U, F>(self, f: F) -> U
-    where
-        F: FnOnce(Self) -> U,
-        Self: Sized,
-    {
-        f(self)
-    }
-}
-
-fn setup_callbacks(pc: &RtcPeerConnection, dc: Option<&RtcDataChannel>, callbacks: PeerConnectionCallbacks) {
-    pc.tap(|pc| setup_ice_callback(pc, callbacks.on_sdp_ready)).tap(|pc| setup_connection_state_callback(pc, callbacks.on_connection_status_change));
-
-    dc.map(|dc| setup_data_channel_callbacks(dc, callbacks.on_connection_established, callbacks.on_message_received));
+    dc.map(|dc| {
+        setup_data_channel_callbacks(
+            dc,
+            callbacks.on_connection_established,
+            callbacks.on_message_received,
+        )
+    });
 }
 
 fn setup_ice_callback(pc: &RtcPeerConnection, mut callback: StringCallback) {
     Closure::wrap(Box::new(move |event: RtcPeerConnectionIceEvent| {
         event.candidate().is_none().then(|| {
-            event.current_target().and_then(|t| t.dyn_into::<RtcPeerConnection>().ok()).and_then(|pc| pc.local_description()).and_then(|desc| js_sys::JSON::stringify(&desc).ok()).and_then(|s| s.as_string()).map(|sdp| {
-                console_log!("generated sdp: {}", sdp);
-                callback(sdp);
-            })
+            event
+                .current_target()
+                .and_then(|t| t.dyn_into::<RtcPeerConnection>().ok())
+                .and_then(|pc| pc.local_description())
+                .and_then(|desc| js_sys::JSON::stringify(&desc).ok())
+                .and_then(|s| s.as_string())
+                .map(|sdp| {
+                    console_log!("generated sdp: {}", sdp);
+                    callback(sdp);
+                })
         });
     }) as Box<dyn FnMut(RtcPeerConnectionIceEvent)>)
     .tap(|closure| pc.set_onicecandidate(Some(closure.as_ref().unchecked_ref())))
@@ -184,8 +202,16 @@ fn setup_connection_state_callback(pc: &RtcPeerConnection, mut callback: StatusC
     .forget();
 }
 
-fn setup_guest_data_channel(pc: &RtcPeerConnection, dc_storage: Rc<RefCell<Option<RtcDataChannel>>>, on_open: SimpleCallback, on_message: StringCallback) -> Result<(), JsValue> {
-    let callbacks = (Rc::new(RefCell::new(Some(on_open))), Rc::new(RefCell::new(Some(on_message))));
+fn setup_guest_data_channel(
+    pc: &RtcPeerConnection,
+    dc_storage: Rc<RefCell<Option<RtcDataChannel>>>,
+    on_open: SimpleCallback,
+    on_message: StringCallback,
+) -> Result<(), JsValue> {
+    let callbacks = (
+        Rc::new(RefCell::new(Some(on_open))),
+        Rc::new(RefCell::new(Some(on_message))),
+    );
 
     Closure::wrap({
         let dc_storage = dc_storage.clone();
@@ -195,10 +221,12 @@ fn setup_guest_data_channel(pc: &RtcPeerConnection, dc_storage: Rc<RefCell<Optio
             e.channel()
                 .tap(|dc| console_log!("data channel created: {}", dc.label()))
                 .tap(|dc| {
-                    (on_open.borrow_mut().take(), on_message.borrow_mut().take()).pipe(|(open_cb, msg_cb)| match (open_cb, msg_cb) {
-                        (Some(open), Some(msg)) => setup_data_channel_callbacks(dc, open, msg),
-                        _ => {}
-                    })
+                    (on_open.borrow_mut().take(), on_message.borrow_mut().take()).pipe(
+                        |(open_cb, msg_cb)| match (open_cb, msg_cb) {
+                            (Some(open), Some(msg)) => setup_data_channel_callbacks(dc, open, msg),
+                            _ => {}
+                        },
+                    )
                 })
                 .pipe(|dc| *dc_storage.borrow_mut() = Some(dc));
         }) as Box<dyn FnMut(RtcDataChannelEvent)>
@@ -209,7 +237,11 @@ fn setup_guest_data_channel(pc: &RtcPeerConnection, dc_storage: Rc<RefCell<Optio
     Ok(())
 }
 
-fn setup_data_channel_callbacks(dc: &RtcDataChannel, mut on_open: SimpleCallback, mut on_message: StringCallback) {
+fn setup_data_channel_callbacks(
+    dc: &RtcDataChannel,
+    mut on_open: SimpleCallback,
+    mut on_message: StringCallback,
+) {
     Closure::wrap(Box::new(move || {
         console_log!("data channel opened");
         on_open();
--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,45 @@ use base64::prelude::*;
 use flate2::{Compression, read::ZlibDecoder, write::ZlibEncoder};
 use std::io::{Read, Write};
 
+/// Performs side effects while preserving the original value
+///
+/// In combinatorial logic: `K x y = x` (Kestrel ignores the second argument)
+/// In our case: `x.tap(f) = { f(&x); x }` (perform f as side effect, return x)
+pub trait Kestrel {
+    fn tap<F>(self, f: F) -> Self
+    where
+        F: FnOnce(&Self);
+}
+impl<T> Kestrel for T {
+    fn tap<F>(self, f: F) -> Self
+    where
+        F: FnOnce(&Self),
+    {
+        f(&self);
+        self
+    }
+}
+
+/// Function application in reverse order  
+///
+/// In combinatorial logic: `T x f = f x` (apply function f to value x)
+/// This is the reverse of normal function application `f(x)`
+pub trait Thrush {
+    fn pipe<U, F>(self, f: F) -> U
+    where
+        F: FnOnce(Self) -> U,
+        Self: Sized;
+}
+impl<T> Thrush for T {
+    fn pipe<U, F>(self, f: F) -> U
+    where
+        F: FnOnce(Self) -> U,
+        Self: Sized,
+    {
+        f(self)
+    }
+}
+
 pub fn compress_string(sdp: &str) -> String {
     let mut encoder = ZlibEncoder::new(Vec::new(), Compression::best());
     encoder.write_all(sdp.as_bytes()).unwrap();
@@ -10,6 +49,8 @@ pub fn compress_string(sdp: &str) -> String {
 
 pub fn decompress_string(compressed_str: &str) -> String {
     let mut decompressed = Vec::new();
-    ZlibDecoder::new(&BASE64_STANDARD.decode(compressed_str).unwrap()[..]).read_to_end(&mut decompressed).unwrap();
+    ZlibDecoder::new(&BASE64_STANDARD.decode(compressed_str).unwrap()[..])
+        .read_to_end(&mut decompressed)
+        .unwrap();
     std::str::from_utf8(&decompressed).unwrap().to_string()
 }
